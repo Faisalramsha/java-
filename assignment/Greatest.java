12de30924e7dc074d2bb02a0e23b71079cec425e
Faisalramsha <98332498+Faisalramsha@users.noreply.github.com>
2022-02-03 20:29:26 -0800

    Add files via upload

--- assignment/Greatest.java
+++ assignment/Greatest.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class Greatest {
 
 	public static void main(String[] args) {
-		int a,b,larger;
+		int a,b,sum,product,avg;
+		int c;
+	
 		Scanner scan = new Scanner(System.in);
 		
 		System.out.println("Enter first number");
@@ -13,13 +15,17 @@ public class Greatest {
 		
 		System.out.println("Enter second number");
 		b = scan.nextInt();
+		System.out.println("Enter third number");
+		c = scan.nextInt();
 		
-		if(a>b)
-			larger=a;
-		else
-			larger=b;
-		
-	System.out.println("the largest number among two =" +larger);	
+		sum=a+b+c   ;
+	System.out.println("The sum of given numbers " +sum);
+	product=a*b*c   ;
+System.out.println("The product of given numbers " +product);
+
+avg=product/3   ;
+System.out.println("The averag of given numbers " +avg);
+	 
 
 	}
 
