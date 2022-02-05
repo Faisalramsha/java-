0c9d2d1b5dd5e79518381330087d6475f40d763e
Faisalramsha <98332498+Faisalramsha@users.noreply.github.com>
2022-02-04 18:18:18 -0800

    Add files via upload

---
+++ practical/Desdcending.java
@@ -0,0 +1,18 @@
+package package_faisal;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Desdcending {
+	public static void main(string[]args) {
+		Integer [] array = {23, -9, 78, 102, 4, 0, -1, 11, 6, 110, 205};   
+		// sorts array[] in descending order   
+		Arrays.sort(array, Collections.reverseOrder());   
+		System.out.println("Array elements in descending order: " +Arrays.toString(array));
+		
+		
+		
+		
+	}
+
+}
