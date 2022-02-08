5e7c8e7f5e0e92d72f3284fb2b0af0116713783a
Faisalramsha <98332498+Faisalramsha@users.noreply.github.com>
2022-02-07 18:13:31 -0800

    Add files via upload

---
+++ assignment/Scope.java
@@ -0,0 +1,19 @@
+package package_faisal;
+
+	class variable_scope
+	 {
+	 public static void main(String args[])
+	 { 
+	int x;
+	x = 5; 
+	{
+	int y = 6; 
+	System.out.print(x + " " + y); 
+	} 
+	
+	System.out.println(x + " " + y);
+	 }
+	 }
+
+
+
