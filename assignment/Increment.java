1a663cfc5c03c4f3f5ebf5432fa517dfa06c974f
Faisalramsha <98332498+Faisalramsha@users.noreply.github.com>
2022-02-07 18:05:59 -0800

    Add files via upload

--- assignment/Increment.java
+++ assignment/Increment.java
@@ -6,9 +6,15 @@ public class Increment {
 	{
 		
 			
-			int g = 3;
-			System.out.print(++g* 8);
-			}}
+		 double a, b,c;
+		 a = 3.0/0;
+		 b = 0/4.0;
+		 c=0/0.0; 
+		System.out.println(a);
+		 System.out.println(b);
+		 System.out.println(c);
+		 } }
+
 
 
 	
