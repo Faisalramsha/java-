ff7a1f7326870e0e64a57ba3e2b476cbe134e0db
Faisalramsha <98332498+Faisalramsha@users.noreply.github.com>
2022-02-08 18:36:58 -0800

    Add files via upload
    new practicals 08-02-2022

---
+++ practical/Linked.java
@@ -0,0 +1,27 @@
+package package_faisal;
+
+import java.util.LinkedList;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
+
+public class Linked {
+
+	public static void main(String[] args) {
+		LinkedList<String> al=new LinkedList<String>();  
+		al.add("Ravi");  
+		al.add("Vijay");  
+		al.add("Ravi");  
+		al.add("Ajay");  
+		Iterator itr=(Iterator) al.iterator();  
+		while(iter.next()){  
+		System.out.println();  
+		}
+		
+		}  
+		
+		
+		
+
+	}
+
+
