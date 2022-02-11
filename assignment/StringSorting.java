b9dd471b2b768c8be5ee930920841910dadcfa11
Faisalramsha <98332498+Faisalramsha@users.noreply.github.com>
2022-02-10 19:23:30 -0800

    Add files via upload

---
+++ assignment/StringSorting.java
@@ -0,0 +1,16 @@
+package package_faisal;
+
+public class StringSorting {
+
+			public static void main(String[] args) {
+				
+				String r = ("faisal");
+				System.out.println(r);
+				
+				
+			}
+	
+
+	}
+
+
