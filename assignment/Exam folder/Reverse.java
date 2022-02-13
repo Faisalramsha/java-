5423c5c4ac12506bb3e9ff105c4ad7201b6adcb2
Faisalramsha <98332498+Faisalramsha@users.noreply.github.com>
2022-02-12 19:21:47 -0800

    Add files via upload

---
+++ assignment/Exam folder/Reverse.java
@@ -0,0 +1,17 @@
+package package_faisal;
+
+public class Reverse {
+	    public static void main(String[] args) {  
+	        int [] arr = new int [] {1, 2, 3, 4, 5};  
+	        System.out.println("Original array: ");  
+	        for (int i = 0; i < arr.length; i++) {  
+	            System.out.print(arr[i] + "        ");  
+	        }  
+	        System.out.println();  
+	        System.out.println("Array in reverse order: ");  
+	        for (int i = 4; i >= 0; i--) {  
+	            System.out.print(arr[i] + "       ");  
+	        }  
+	    }  
+	}  
+
