1df0ff9a95d839688ec2046573cb497d3dc26753
Faisalramsha <98332498+Faisalramsha@users.noreply.github.com>
2022-02-12 18:41:03 -0800

    Add files via upload

---
+++ assignment/Exam folder/Sum.java
@@ -0,0 +1,15 @@
+package package_faisal;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		int arr[]= {1,4,6,2,4};
+		int sum=0;
+		
+		for(int i: arr)
+				sum+=i;
+			System.out.println("/nThe sum of given array are  ---->>>>>   "+sum);
+
+	}
+
+}
