fa657059300f9aa5cc3503cbbeb84832f1ed0fbb
Faisalramsha <98332498+Faisalramsha@users.noreply.github.com>
2022-03-06 10:09:05 +0530

    Add files via upload

---
+++ practical/TryCatch.java
@@ -0,0 +1,21 @@
+package packege_faisal;
+
+public class TryCatch {
+
+	    public static void main(String[] args) {  
+	        try  
+	        {  
+	        int data=-20/0; 
+	        }  
+	            
+	        catch(Exception e)  
+	        {  
+	                   
+	            System.out.println("Number should not be negative");  
+	        }  
+	    }  
+	      
+	}  
+	
+
+
