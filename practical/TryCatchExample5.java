110913565122fd0e8f239f15e42102b956da810f
Faisalramsha <98332498+Faisalramsha@users.noreply.github.com>
2022-03-06 09:50:09 +0530

    Add files via upload

---
+++ practical/TryCatchExample5.java
@@ -0,0 +1,19 @@
+package package_faisal;
+
+
+public class TryCatchExample5 {  
+	  
+    public static void main(String[] args) {  
+        try  
+        {  
+        int data=50/0; 
+        }  
+            
+        catch(Exception e)  
+        {  
+                   
+            System.out.println("Number should not be zero");  
+        }  
+    }  
+      
+}  
